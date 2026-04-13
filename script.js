1a0de1ee0e438b1becb7d7978fa4d3280e6513ca
federicosimone <federico.simone15@gmail.com>
2026-04-13 17:15:12 +0200

    Q3

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 
 //console.log(hamburger.name) => "Double Cheese Burger"
 //console.log(secondBurger.name) => "Double Chees Burger"
+
+/*---------------------------------------------------------- */
+
+//🏆 Code Question 2
+
+//console.log(hamburger.ingredients[0]) => "Salad"
+//console.log(secondBurger.ingredients[0]); => "Salad"
+
+/*---------------------------------------------------------- */
+
+//🏆 Code Question 3
+
+// Sono stati creati 9 oggetti, considerando hamburger 
